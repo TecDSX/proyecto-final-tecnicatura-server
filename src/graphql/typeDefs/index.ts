0fa229b985f53e388dce7ddb383202a09967074c
Damian <damianezetiel1@gmail.com>
2020-04-13 22:00:34 -0300

    remove all files

--- src/graphql/typeDefs/index.ts
+++
@@ -1,4 +0,0 @@
-import { fileLoader, mergeTypes } from 'merge-graphql-schemas';
-import path from 'path';
-const typesArray = fileLoader(path.join(__dirname, '.'));
-export const typeDefs = mergeTypes(typesArray, { all: true });
